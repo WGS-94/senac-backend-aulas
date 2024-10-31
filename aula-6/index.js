ab378e354a9ba6502e550c62b3d2028b8a267341
WGS-94 <wilson-senx@hotmail.com>
2024-10-30 21:14:15 -0300

    :fire: Continuação aula 6 sobre construção da primeira api

--- aula-6/index.js
+++ aula-6/index.js
@@ -6,22 +6,61 @@ const http = require('http');
 // que é passada como parâmetro para http.createServer(), iniciamos a arrow function
 http.createServer((req, res) => {
 
-    // res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("Hello World");
+    // Extraindo a URL da requisição
+    const url = req.url;
+    const method = req.method;
 
-    console.log("Requisição recebida!");
-    console.log("Parâmetros da requisição:");
+    console.log(url, method);
+
+    if(url.startsWith('/dados')){
+        if(method === "GET"){
+            const queryParams = new URLSearchParams(req.url.split("?")[1]);
+            // Obter o valor do parâmetro 'nome'
+            const nome = queryParams.get('nome')
+            // Obter o valor do parâmetro 'idade'
+            const idade = queryParams.get('idade')
+
+            // Preparando resposta com os dados recebidos
+            const respostaData = {
+                nome:  nome,
+                idade: idade,
+                mensagem: `Olá, ${nome}! Você tem ${idade} anos de idade.`
+            }
+
+            // Definindo cabeçalho de resposta como json
+            res.setHeader("Content-Type", "application/json");
+
+            // Envia a resposta com os status (OK) e o corpo da resposta em JSON
+            res.statusCode = 200
+            res.end(JSON.stringify(respostaData))
+        }
+        else if(method === "POST"){
+            const body = "";  // Armazena o corpo da requisição
+
+            //  Escuta de dados que são enviado no corpo da requisição
+
+            req.on('data', chunk => {
+                body += chunk.toString();
+            })
+        }
+    }
+
+
+    // res.write("Hello World");
+
+    // console.log("Requisição recebida!");
+    // console.log("Parâmetros da requisição:");
   
-    // Mostra o método da requisição (GET, POST, PUT, DELETE, etc.)
-    console.log("Método:", req.method);
+    // // Mostra o método da requisição (GET, POST, PUT, DELETE, etc.)
+    // console.log("Método:", req.method);
   
-    // Mostra a URL da requisição
-    console.log("URL:", req.url);
+    // // Mostra a URL da requisição
+    // console.log("URL:", req.url);
   
-    // // Mostra os cabeçalhos da requisição
-    console.log("Cabeçalhos:", req.headers);
+    // // // Mostra os cabeçalhos da requisição
+    // console.log("Cabeçalhos:", req.headers);
   
-    res.end('Fim');
+    // res.end('Fim');
 }).listen(9090, () => {
     console.log('Servidor rodando na porta 9090...');
 });
