3e93d0c33998a5b1ac40639c8b613d41a85c98a4
WGS-94 <wilson-senx@hotmail.com>
2024-11-19 19:54:19 -0300

    :saprkles: Continuaçãp sobre a aula de typescript

---
+++ aula-11/anaimal.ts
@@ -0,0 +1,39 @@
+// Vamos desenvolver uma api básica 
+
+// classe Pai - Animal (base)
+
+class Animal {
+    // propriedades básicas que todos os animais terão
+    nome: string;
+    idade: number;
+    especie: string;
+    raca: string;
+    peso: number;
+
+    // Constructor da classe Animal (inicializa as propriedades quando um novo objeto é criado) 
+    constructor( nome: string, idade: number, especie: string, raca: string, peso: number){
+        this.nome = nome;
+        this.idade = idade;
+        this.especie = especie;
+        this.raca = raca;
+        this.peso = peso;
+    }
+
+    // Método emitirSim(): cada tipo de animal pode ter um som específico
+    emitirSom(): string {
+        return "Som do animal (genérico)" // Som que qualquer animal emite 
+    }
+
+    // Método interagir(): Simular a interção com o animal (ex: alimentar, brincar)
+    interagir(acao: string): string {
+        // dependendo da ação, o retorno muda
+        if(acao === 'alimentar'){
+            return `${this.nome} está comendo!`;
+        }else if(acao === 'brincar'){
+            return `${this.nome} está brincando!`;
+        }else {
+            return `${this.nome} está descansando!`;
+        }
+    }
+}
+
