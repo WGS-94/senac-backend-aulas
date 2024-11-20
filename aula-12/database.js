3525de7c2b5c22363880e27957749cf64212ee2e
WGS-94 <wilson-senx@hotmail.com>
2024-11-19 21:13:32 -0300

    :tada: Introdução ao Banco de Dados

---
+++ aula-12/database.js
@@ -0,0 +1,23 @@
+// ============== INTRODUÇÃO AO BANCO DE DADOS ==============
+
+/*
+    Banco de Dados: Coleção organizada de infos ou dados, que podem ser acessados, 
+    gerenciados, atualizados e manipulados.
+
+    Ex: Agenda de contatos de celulares (armazenamos números, nome, etc)
+
+    Caracteristicas de um Banco de Dados:
+
+    - Organização
+    - Gerenciamento
+    - Acessibilidade (acesso)
+    - Manipulação (inserção, alteração, exclusão)
+    - Escabilidade (ampliação)
+    - Segurança (autenticação, autorização, etc)
+    - Persistência (armazenamento)
+
+    Tipos de Bando de Dados:
+
+    - Relacional (MySQL, PostgreSQL)
+    - Não Relacional (MongoDB, Cassandra, NoSQL, Neo4)
+*/
