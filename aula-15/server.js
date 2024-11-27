c634db204cdcf384f9d85a79aa71bbccdcec76a5
WGS-94 <wilson-senx@hotmail.com>
2024-11-27 20:29:45 -0300

    :fire: Criando instância do app

---
+++ aula-15/server.js
@@ -0,0 +1,7 @@
+// Importa os módulos necessários para o funcionamento do servidor
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+// Cria uma instância do servidor Express
+const app = express();
